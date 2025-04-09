13f2ae4b5309c9cdfcf86f1e0614c0a8313ee748
MarcusBlaisdell <marcus.blaisdell@gmail.com>
2025-04-09 14:21:47 -0700

    added test cases

--- coinChange.py
+++ coinChange.py
@@ -20,11 +20,20 @@ def coinChange(i: List[int], a: int) -> int:
     return res
 
 def main() -> None:
-    input = [([1,2,5],11)]
+    input = [[([1,2,5],11), 3],
+            [([1,2,5], 12), 3],
+            [([1,2,5], 13), 4],
+            [([1,2,5], 14), 4],
+            [([1,2,5], 15), 3],
+            [([1,2,5], 16), 4],
+            [([1,2,5], 17), 4],
+            [([1,2,5], 18), 5],
+            [([1,2,5], 19), 5],
+            [([1,2,5], 20), 4],]
 
-    for i, a in input:
-        a = coinChange(i, a)
-        print("answer: ", a)
+    for x in input:
+        a = coinChange(x[0][0], x[0][1])
+        print("answer: ", a, " s/b: ", x[1], " Match: ", a == x[1])
 
 
 if __name__=='__main__':
