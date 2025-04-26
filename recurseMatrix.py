4f583d29a7e6626aa112b494031b65d42352f73d
MarcusBlaisdell <marcus.blaisdell@gmail.com>
2025-04-25 18:01:45 -0700

    fixed recursive print test

--- recurseMatrix.py
+++ recurseMatrix.py
@@ -1,5 +1,32 @@
 '''
 Printing contents of a matrix recursively:
+2
+- 2
+3
+- 3
+4
+- 4
+6
+7
+- 7
+- 7
+8
+- 8
+- 8
+9
+- 9
+- 9
+11
+12
+- 12
+- 12
+13
+- 13
+- 13
+14
+- 14
+- 14
+missing: 1, 5, 10, 15, 16, 17, 18, 19, 20
 '''
 from typing import List
 
@@ -10,15 +37,16 @@ def printMatrixHelp(grid: List, node: tuple) -> None:
         pass
     else:
         visited.add(node)
+        print(grid[node[0]][node[1]])
         if (node[1] - 1) > -1:
             printMatrixHelp(grid, (node[0], node[1] - 1))
-            print(grid[node[0]][node[1]])
-        if (node[0] + 1) < (r - 1):
+            #print(grid[node[0]][node[1]])
+        if (node[0] + 1) < r:
             printMatrixHelp(grid, (node[0] + 1,node[1]))
-            print(grid[node[0] + 1][node[1]])
-        if (node[1] + 1) < (c - 1):
+            #print(grid[node[0] + 1][node[1]])
+        if (node[1] + 1) < c:
             printMatrixHelp(grid, (node[0], node[1] + 1))
-            print(grid[node[0]][node[1] + 1])
+            #print(grid[node[0]][node[1] + 1])
 
 def printMatrix(input: List[List[str]]):
     global visited
