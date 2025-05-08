472c339b497b231a304248f613fb9ee9551e5116
MarcusBlaisdell <marcus.blaisdell@gmail.com>
2025-05-07 20:04:19 -0700

    Added square to pygame test

--- pygameIntro.py
+++ pygameIntro.py
@@ -9,7 +9,8 @@ import random
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 
-circle_pos = (1280/2, 720/2)
+circle_pos = (random.randint(50,1230), random.randint(50,670))
+rect_pos = (random.randint(0,1230), random.randint(0,670), 50, 50)
 
 font = pygame.font.Font(None, 30)
 
@@ -20,6 +21,13 @@ def checkCircleCollision() -> bool:
     if math.sqrt(((mouse_pos[0] - circle_pos[0])**2) + ((mouse_pos[1] - circle_pos[1])**2)) <= 50:
         return True
     return False
+
+def checkRectCollision() -> bool:
+    mouse_pos = pygame.mouse.get_pos()
+    if (mouse_pos[0] >= rect_pos[0]) and (mouse_pos[0] <= (rect_pos[0] + 50)):
+        if (mouse_pos[1] >= rect_pos[1]) and (mouse_pos[1] <= (rect_pos[1] + 50)):
+            return True
+        return False
 
 while True:
     events = pygame.event.get()
@@ -32,14 +40,18 @@ while True:
                 if checkCircleCollision():
                     circle_pos = (random.randint(50,1230), random.randint(50,670))
                     score += 1
-                else:
-                    score -= 1
+                #else:
+                    #score -= 1
+                if checkRectCollision():
+                    rect_pos = (random.randint(0,1230), random.randint(0,670), 50, 50)
+                    score += 1
 
     scoreObj = font.render(f'Score: {score}', True, 'black')
 
 
     screen.fill('lightblue')
     pygame.draw.circle(screen, "red", circle_pos, 50)
+    pygame.draw.rect(screen, "green", pygame.Rect(rect_pos))
     screen.blit(scoreObj, (50,50))
 
     pygame.display.update()
