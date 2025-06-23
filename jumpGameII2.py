214a1b9cd5d8313bc80aad522dd57fc37f8930f4
MarcusBlaisdell <marcus.blaisdell@gmail.com>
2025-06-23 13:53:33 -0700

    Solved Jump Game

--- jumpGameII2.py
+++ jumpGameII2.py
@@ -38,7 +38,9 @@ Constraints:
     0 <= nums[i] <= 1000
     It's guaranteed that you can reach nums[n - 1].
 
-
+110/110
+Runtime beats 92.93%
+Memory beats 84.73%
 '''
 from typing import List
 import time
@@ -50,31 +52,31 @@ def jump(nums: List) -> int:
         """
         if (len(nums) == 1):
             return 0
-        i = 0
-        j = 0
+        i = 0 # index
+        j = 0 # jump counter
 
-        while i <= len(nums):
-            print("i: ", i)
-            if i >= len(nums) - 1:
+        while i < (len(nums) - 1):
+            if (i + nums[i]) >= (len(nums) - 1):
                 return j + 1
-            if i + nums[i] >= (len(nums) - 1):
-                return j + 1
-            else:
-                for k in range(nums[i]):
-                    m = 0
-                    if (i + k + nums[i + k]) > m:
-                        m = i + k + nums[i + k]
-                        n = i + k
-                if n == i:
-                    return j + 1
-                else:
-                    i = n
+            m = 0 # max val
+            n = 0
+            # find optimal jump position,
+            # defined as:
+            # position that gets farthest
+
+            for k in range(1,nums[i] + 1):
+                if ((i + k + nums[i + k]) > m) and (nums[i + k] != 0):
+                    m = i + k + nums[i + k]
+                    n = i + k # next index
+            i = n
             j += 1
 
         return j
 
 def main() -> None:
     t1 = time.time()
+    correct = 0
+    total = 0
     input = [([2,3,1,1,4], 2),
             ([2,3,0,1,4], 2),
             ([0], 0),
@@ -85,9 +87,13 @@ def main() -> None:
             ([3,4,3,2,5,4,3],3)]
 
     for i in input:
+        total += 1
         a = jump(i[0])
+        if a == i[1]:
+            correct += 1
         print(i[0], ", s/b: ", i[1], ", is: ", a)
 
+    print("Total: ", correct, " / ", total)
     print("Total time: ", time.time() - t1)
 
 if __name__=='__main__':
