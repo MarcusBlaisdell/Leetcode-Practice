1313ca032f0b496f4236a147998fd33345d2a45c
MarcusBlaisdell <marcus.blaisdell@gmail.com>
2025-07-01 13:36:55 -0700

    test new token

--- rotateImage.py
+++ rotateImage.py
@@ -56,23 +56,24 @@ Constraints:
 import time
 from typing import List
 
-def rotate(matrix: List) -> List:
+def rotate(matrix: List[List[int]]) -> None:
         """
         :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        :rtype: None Do not return anything, modify matrix
+         in-place instead.
         """
 
 def main() -> None:
     t1 = time.time()
-    
+
     input = [([[1,2,3],[4,5,6],[7,8,9]],
             [[7,4,1],[8,5,2],[9,6,3]]),
             ([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]],
             [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]])]
 
     for i in input:
-        a = rotate(i[0])
-        print("s/b: ", i[1], ", is: ", a)
+        rotate(i[0])
+        print("s/b: ", i[1], ", is: ", i[0])
 
     print("Total time: ", time.time() - t1)
 
